f3f4748521bdfb243ac350e62b258d97dd60c9e4
SRead <39156178+SLRead@users.noreply.github.com>
2021-10-26 21:32:45 +0100

    Implement interface

--- contracts/Scribe.sol
+++ contracts/Scribe.sol
@@ -1,33 +1,9 @@
 pragma solidity ^0.5.12;
 
-// TODO: import erC20? Or import special DADA modified ERC-20 possible?
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
-/** 
- * Utilities library
- */
-//library Utilities {
-	// concat two bytes objects
-  //  function concat(bytes memory a, bytes memory b)
-    //        internal pure returns (bytes memory) {
-      //  return abi.encodePacked(a, b);
-  //  }
-
-    // convert address to bytes
-  //  function toBytes(address x) internal pure returns (bytes memory b) { 
-	//	b = new bytes(20); 
-	
-	//	for (uint i = 0; i < 20; i++) 
-	//		b[i] = byte(uint8(uint(x) / (2**(8*(19 - i))))); 
-//	}
-
-	// convert uint256 to bytes
-//	function toBytes(uint256 x) internal pure returns (bytes memory b) {
-  //  	b = new bytes(32);
-    //	assembly { mstore(add(b, 32), x) }
-//	}
-//}
-
+interface DadaCollectible {
+    function DrawingPrintToAddress(uint256) external view returns (address);
+}
 
 /*
  * @title Contract that allows an owner of a Creep to scribe a message attached to the token.
@@ -37,7 +13,8 @@ import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol"
  * Modified by sparrow
  */
 contract Scribe {
-address creepsContract = 0x068696a3cf3c4676b65f1c9975dd094260109d02;
+//address creepsContract = 0x068696A3cf3c4676B65F1c9975dd094260109d02;
+address creepsContract = 0xbc2Df256FA6FAd53BfBf0a054aBF43561AcAafe3;
 // A record event that emits each time an owner dictates a message
 	event Record (
 	// the address of who dicated this document
@@ -47,6 +24,8 @@ address creepsContract = 0x068696a3cf3c4676b65f1c9975dd094260109d02;
         // The text of the dictation
         string text
     );
+    
+    uint256 creepOwner;
 
 	// A recorded document which tracks the dictator, the text, and the timestamp of when it was created
 	struct Document {
@@ -57,6 +36,11 @@ address creepsContract = 0x068696a3cf3c4676b65f1c9975dd094260109d02;
 		// the block time of the dictation
 		uint creationTime;
 	}
+	
+	function getOwner(uint256 printIndex) public view returns (address) {
+     return DadaCollectible(creepsContract).DrawingPrintToAddress(printIndex);
+     
+    }
 
 	// Mapping of document keys to documents (keys are concated token address + tokenId)
 	mapping (uint256 => Document[]) public documents;
@@ -69,7 +53,7 @@ address creepsContract = 0x068696a3cf3c4676b65f1c9975dd094260109d02;
 		// check that the message sender owns the token 
 		// the function in DadaCollectible.sol that returns the owner address given a unique printIndex
 		// is DrawingPrintToAddress
-		require(DrawingPrintToAddress(printIndex) == msg.sender, "Sender not authorized to dictate.");
+		require(getOwner(printIndex) == msg.sender, "Sender not authorized to dictate.");
 
 		// push a new document with the dictator address, message, and timestamp
 		documents[printIndex].push(Document(msg.sender, _text, block.timestamp));
